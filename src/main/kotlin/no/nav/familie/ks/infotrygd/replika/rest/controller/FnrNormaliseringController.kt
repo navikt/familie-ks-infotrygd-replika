b120312a6075325eb0d480bc4a94afa8f7879b87
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2026-09-24 14:05:13 +0200

    Eget endepunkt for seq-dato-konvertering per kolonne

--- src/main/kotlin/no/nav/familie/ks/infotrygd/replika/rest/controller/FnrNormaliseringController.kt
+++ src/main/kotlin/no/nav/familie/ks/infotrygd/replika/rest/controller/FnrNormaliseringController.kt
@@ -2,8 +2,6 @@ package no.nav.familie.ks.infotrygd.replika.rest.controller
 
 import no.nav.familie.ks.infotrygd.replika.normalisering.FnrKolonne
 import no.nav.familie.ks.infotrygd.replika.normalisering.FnrNormaliseringRepository
-import no.nav.familie.ks.infotrygd.replika.normalisering.SeqDatoKolonne
-import no.nav.familie.ks.infotrygd.replika.normalisering.SeqDatoRepository
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.PostMapping
@@ -11,21 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 data class FnrNormaliseringRespons(
-    val antallOppdatert: Map<String, Int>,
+    val antallOppdatert: Int,
 )
 
 @PreAuthorize("hasRole('FORVALTER')")
 @RestController
 @RequestMapping("/api/admin/fnr-normalisering")
 class FnrNormaliseringController(
-    private val fnrNormaliseringRepository: FnrNormaliseringRepository,
-    private val seqDatoRepository: SeqDatoRepository,
+    private val repository: FnrNormaliseringRepository,
 ) {
     @PostMapping
     @Transactional
-    fun normaliser(): FnrNormaliseringRespons {
-        val fnrResultater = FnrKolonne.entries.associate { it.målkolonne to fnrNormaliseringRepository.normaliser(it) }
-        val seqDatoResultater = SeqDatoKolonne.entries.associate { it.målkolonne to seqDatoRepository.konverter(it) }
-        return FnrNormaliseringRespons(fnrResultater + seqDatoResultater)
-    }
+    fun normaliser(): FnrNormaliseringRespons = FnrNormaliseringRespons(repository.normaliser(FnrKolonne.KS_BARN_10))
 }
